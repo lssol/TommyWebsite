9fac055ef24708cc4425ff196a94efdd2927ebda
Sacha Brisset <sacha.brisset@hotmail.fr>
2018-09-09 11:27:37 +0200

    Finished the mobile menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import {Component, HostListener} from '@angular/core';
+import {Component, HostListener, Input, OnChanges, SimpleChanges} from '@angular/core';
 import { fadeAnimation } from './app.animation';
+import * from "jquery"
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +10,28 @@ import { fadeAnimation } from './app.animation';
   animations: [ fadeAnimation ]
 })
 export class AppComponent {
+  router: Router;
   reduceHeader = false;
+  @Input() menuActivated: boolean;
   @HostListener('window:scroll') onScroll() {
       this.reduceHeader = window.scrollY > 0;
   }
   getActivatedRoute(o): string {
     return o.isActivated ? o.activatedRoute + o.activatedRoute.snapshot.queryParams['category'] : '';
+  }
+
+  constructor(router: Router) {
+    this.router = router;
+    this.router.events.subscribe((val) => this.toggleScrolling(false))
+  }
+
+  toggleScrolling(newValue): void {
+    this.menuActivated = newValue;
+    if (this.menuActivated) {
+      $('body').addClass('stop-scrolling');
+    } else {
+      $('body').removeClass('stop-scrolling');
+    }
   }
 }
 
